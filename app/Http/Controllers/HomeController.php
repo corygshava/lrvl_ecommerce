a26fb2aa7b8609a15b073bb6fd2c18d21df40efe
corygshava <corygshava@gmail.com>
2025-09-22 14:45:08 +0300

    tutorial 2, half complete

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function send_to_dash() {
+        $usertype = auth()->user()->usertype;
+
+        // echo "<h1>$usertype</h1>";
+
+        if($usertype == '1'){
+            // echo 'welcome admin';
+
+            return view('admin.home');
+        } else {
+            // echo "welcome customer";
+
+            return view('dashboard');
+        }
+    }
+}
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,21 @@
+<?php
+    $header = "admin dash"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Dash</title>
+</head>
+<body>
+    <h1>Admin Dash</h1>
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {{ $header }}
+        </div>
+    </header>
+</body>
+</html>
