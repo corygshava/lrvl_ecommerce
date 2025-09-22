9527ae475af10c04cd184c7da28afc80667cc300
corygshava <corygshava@gmail.com>
2025-09-22 14:58:18 +0300

    tutorial 2 finished: added admin and customer login

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,21 +1,10 @@
-<?php
-    $header = "admin dash"
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin Dash</title>
-</head>
-<body>
-    <h1>Admin Dash</h1>
-    <header class="bg-white shadow">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            {{ $header }}
-        </div>
-    </header>
-</body>
-</html>
+<x-app-layout>
+    <x-slot>
+        
+    </x-slot>
+    {{-- <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot> --}}
+</x-app-layout>
